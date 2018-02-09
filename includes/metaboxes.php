62af0b3380ae31b8ce389cf76e154af3a8407838
orlando reynoso job <orlandoreynoso01@gmail.com>
2018-02-09 17:54:05 -0600

    add nonce

--- includes/metaboxes.php
+++ includes/metaboxes.php
@@ -18,11 +18,148 @@ add_action('add_meta_boxes','quizbook_agregar_metaboxes');
 * Muetra el contenido HTML del Metaboxes
 */
 
-function quizbook_metaboxes(){
+function quizbook_metaboxes($post){ 
 
-	echo "Hola Metaboxes";
+	wp_nonce_field( basename(__FILE__), 'quizbook_nonce');
+
+?>
+
+<table class="form-table">
+	<tr>
+		<th class="row-title">
+			<h2>Añade las respuestas Aqui</h2>			
+		</th>
+	</tr>
+	<tr>
+		<th class="row-title">
+			<label for="respuesta_1">a)</label>
+		</th>
+		<td>
+			<input value= "<?php echo get_post_meta( $post->ID, 'qb_respuesta_1',true ); ?>" type="text" id="respuesta_1" name = "qb_respuesta_1" class="regular-text">
+		</td>
+	</tr>
+	<tr>
+		<th class="row-title">
+			<label for="respuesta_2">b)</label>
+		</th>
+		<td>
+			<input value= "<?php echo get_post_meta( $post->ID, 'qb_respuesta_2',true ); ?>" type="text" id="respuesta_2" name = "qb_respuesta_2" class="regular-text">
+		</td>
+	</tr>
+	<tr>
+		<th class="row-title">
+			<label for="respuesta_3">c)</label>
+		</th>
+		<td>
+			<input value= "<?php echo get_post_meta( $post->ID, 'qb_respuesta_3',true ); ?>" type="text" id="respuesta_3" name = "qb_respuesta_3" class="regular-text">
+		</td>
+	</tr>
+	<tr>
+		<th class="row-title">
+			<label for="respuesta_4">d)</label>
+		</th>
+		<td>
+			<input value= "<?php echo get_post_meta( $post->ID, 'qb_respuesta_4',true ); ?>" type="text" id="respuesta_4" name = "qb_respuesta_4" class="regular-text">
+		</td>
+	</tr>
+	<tr>
+		<th class="row-title">
+			<label for="respuesta_5">e)</label>
+		</th>
+		<td>
+			<input value= "<?php echo get_post_meta( $post->ID, 'qb_respuesta_5',true ); ?>" type="text" id="respuesta_5" name = "qb_respuesta_5" class="regular-text">
+		</td>
+	</tr>
+	<tr>
+		<th class="row-title">
+			<label for="respuesta_correcta">Respuesta correcta</label>
+		</th>
+		<td>
+			<?php $respuesta = get_post_meta( $post->ID, 'quizbook_correcta',true ); ?>
+			<select name="quizbook_correcta" id="respuesta_correcta" class="postbox">
+				<option value="">Elige la respuesta correcta</option>
+				<option <?php selected( $respuesta, 'qb_respuesta:a' ); ?> value="qb_respuesta:a">a</option>
+				<option <?php selected( $respuesta, 'qb_respuesta:b' ); ?> value="qb_respuesta:b">b</option>
+				<option <?php selected( $respuesta, 'qb_respuesta:c' ); ?> value="qb_respuesta:c">c</option>
+				<option <?php selected( $respuesta, 'qb_respuesta:d' ); ?> value="qb_respuesta:d">d</option>
+				<option <?php selected( $respuesta, 'qb_respuesta:e' ); ?> value="qb_respuesta:e">e</option>																
+			</select>
+		</td>
+	</tr>	
+</table>
+
+	<?php
 }
 
+/*
+* al save post, se le debe de pasar 3 parámetros
+*
+*/
+
+function quizbook_guardar_metaboxes($post_id,$post,$update){
+
+	if (!isset($_POST['quizbook_nonce']) || !wp_verify_nonce( $_POST['quizbook_nonce'], basename(__FILE__) )) {
+		return $post_id;
+	}
+
+	if (!current_user_can( 'edit_post', $post_id)) {
+		return $post_id;
+	}
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		return $post_id;
+	}
+
+	$respuesta_1 = $respuesta_2 = $respuesta_3 = $respuesta_4 = $respuesta_5 = $correcta = '';
+
+	if (isset($_POST['qb_respuesta_1'])) {
+		$respuesta_1 = sanitize_text_field( $_POST['qb_respuesta_1'] );
+		
+	}
+	update_post_meta($post_id,'qb_respuesta_1',$respuesta_1);
+
+
+
+	if (isset($_POST['qb_respuesta_2'])) {
+		$respuesta_2 = sanitize_text_field( $_POST['qb_respuesta_2'] );
+		
+	}
+	update_post_meta($post_id,'qb_respuesta_2',$respuesta_2);	
+
+
+	if (isset($_POST['qb_respuesta_3'])) {
+		$respuesta_3 = sanitize_text_field( $_POST['qb_respuesta_3'] );
+		
+	}
+	update_post_meta($post_id,'qb_respuesta_3',$respuesta_3);
+
+
+	if (isset($_POST['qb_respuesta_4'])) {
+		$respuesta_4 = sanitize_text_field( $_POST['qb_respuesta_4'] );
+		
+	}
+	update_post_meta($post_id,'qb_respuesta_4',$respuesta_4);
+
+
+	if (isset($_POST['qb_respuesta_5'])) {
+		$respuesta_5 = sanitize_text_field( $_POST['qb_respuesta_5'] );
+		
+	}
+
+	update_post_meta($post_id,'qb_respuesta_5',$respuesta_5);			
+
+	if (isset($_POST['quizbook_correcta'])) {
+		$correcta = sanitize_text_field( $_POST['quizbook_correcta'] );
+		
+	}
+	update_post_meta($post_id,'quizbook_correcta',$correcta);		
+
+}
+
+add_action( 'save_post', 'quizbook_guardar_metaboxes', 10, 3);
+
+/*
+* Metaboxes CMB2
+*/
 
 function quizbook_campos_quiz() {
 	$prefix = 'info_quiz_';
